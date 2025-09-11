e728ab9714acb18ef82da973b0693696e3fdb690
Dennis Iversen <deiv@aarhus.dk>
2025-09-11 09:06:18 +0200

    shift to new dev url.

--- tests/settings_test.py
+++ tests/settings_test.py
@@ -5,7 +5,7 @@ settings: dict[str, typing.Any] = {
     "client_name": "test",
     "client_url": "https://demo.openaws.dk",
     "log_handlers": ["stream"],
-    "api_base_url": "https://staging.openaws.dk/v1",
+    "api_base_url": "https://dev.openaws.dk/v1",
     "sqlite3": {
         "default": "/tmp/database.db",
         "orders": "/tmp/orders.db",
