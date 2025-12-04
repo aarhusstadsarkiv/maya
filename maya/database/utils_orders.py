7a8f3da890ad4c9215ba55318ebe14615bbd0bcc
Dennis Iversen <deiv@aarhus.dk>
2025-12-04 12:37:34 +0100

    update texts

--- maya/database/utils_orders.py
+++ maya/database/utils_orders.py
@@ -76,9 +76,9 @@ def get_single_order_message(order: dict) -> str:
     if order["order_status"] == ORDER_STATUS.ORDERED:
         message = "Materialet er bestilt. Du modtager en e-mail fra os, når materialet er klar til gennemsyn."
         if order["location"] == RECORD_LOCATION.READING_ROOM:
-            message = "Dit bestilte materialle er tilgængeligt. Du kan gennemse det på læsesalen."
+            message = "Det bestilte materialle er tilgængeligt. Du kan gennemse det på læsesalen."
     elif order["order_status"] == ORDER_STATUS.QUEUED:
-        message = "Din bestilling er i kø. Du modtager en mail når den er klar."
+        message = "Din bestilling er i kø. Du modtager en e-mail fra os, når materialet er klar til gennemsyn."
 
     return message
 
