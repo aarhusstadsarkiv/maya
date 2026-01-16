090f990b0677ed40642fa67f95d040a1d5fa3e06
Dennis Iversen <deiv@aarhus.dk>
2026-01-16 14:36:11 +0100

    update lydspor settings

--- sites/lydspor/settings.py
+++ sites/lydspor/settings.py
@@ -3,11 +3,11 @@ import typing
 
 settings: dict[str, typing.Any] = {
     "client_name": "development",
-    "client_url": "https://demo.openaws.dk",
+    "client_url": "https://www.lydsporaarhus.dk",
     "client_email": "stadsarkivet@aarhusarkivet.dk",
     "language": "da",
     "log_handlers": ["rotating_file"],
-    "api_base_url": "https://dev.openaws.dk/v1",
+    "api_base_url": "https://api.openaws.dk/v1",
     "main_menu_top": [
         {"name": "search_get", "title": "Søg", "type": "icon", "icon": "search"},
     ],
