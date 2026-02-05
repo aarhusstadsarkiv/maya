d15eb6a49a6723ac5d9f3ae31397917dfeed913e
Dennis Iversen <deiv@aarhus.dk>
2026-02-05 14:19:20 +0100

    only show if site has admin_tag lydspor

--- sites/lydspor/hooks.py
+++ sites/lydspor/hooks.py
@@ -32,13 +32,20 @@ class Hooks(HooksSpec):
         Alter the search query params. Before the search is executed.
         This example removes all curators from the query params and adds Aarhus Teater as curator (4).
         """
+        # Remove all curators from the query params and add curator (4)
+        query_params = [(key, value) for key, value in query_params if key != "admin_tags"]
+        query_params.append(("admin_tags", "Lydspor"))
 
         return query_params
 
     async def after_get_search(self, query_params: list) -> list:
         """
         Alter the search query params. After the search is executed.
+        This example removes all curators from the query params.
+        This is done to avoid that the curator added in the before_search method is added to filters and search cookie.
         """
+        query_params = [(key, value) for key, value in query_params if key != "admin_tags"]
+
         return query_params
 
     async def after_get_record(self, record: dict, meta_data: dict) -> tuple:
