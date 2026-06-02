82441164ba9983b68b9d8dd3469d66cd3ea27f3c
Dennis Iversen <deiv@aarhus.dk>
2026-06-02 10:16:55 +0200

    clean up salling hooks

--- sites/salling/hooks.py
+++ sites/salling/hooks.py
@@ -1,6 +1,5 @@
 from maya.core.logging import get_log
 from maya.core.hooks_spec import HooksSpec
-from maya.records import record_utils
 
 log = get_log()
 
@@ -49,11 +48,6 @@ class Hooks(HooksSpec):
         query_params = [(key, value) for key, value in query_params if key != "organisations"]
         query_params.append(("organisations", "107434"))
 
-        # online
-        # query_params.append(("availability", "4"))
-        # images
-        # query_params.append(("content_types", "61"))
-
         return query_params
 
     async def after_get_search(self, query_params: list) -> list:
@@ -64,34 +58,4 @@ class Hooks(HooksSpec):
         """
         # organisations=107434
         query_params = [(key, value) for key, value in query_params if key != "organisations"]
-
-        # online
-        # query_params = [(key, value) for key, value in query_params if key != "availability"]
-        # images
-        # query_params = [(key, value) for key, value in query_params if key != "content_types"]
-
         return query_params
-
-    async def after_get_record(self, record: dict, meta_data: dict) -> tuple:
-        """
-        Alter the record and meta_data dictionaries after the api call
-        """
-        # sejrs sedler
-        if record_utils.is_collection(record, 1):
-            meta_data["title"] = ""
-            meta_data["record_type"] = "sejrs_sedler"
-            meta_data["representation_text"] = record.get("summary", "")
-            if record.get("summary"):
-                del record["summary"]
-            else:
-                record_id = record.get("id")
-                assert isinstance(record_id, str)
-                extra = {"error_code": 499, "error_url": record_utils.get_record_url(record_id)}
-                log.error("Sejrs sedler should have a summary", extra=extra)
-
-        # teater arkivet
-        if record_utils.is_curator(record, 4):
-            if record.get("summary"):
-                meta_data["title"] = f"[{record['summary']}]"
-
-        return record, meta_data
